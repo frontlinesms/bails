9ab398fa50c0609420dc6296b36bb237ec9e4306
Alex Anderson <alex@frontlinesms.com>
2013-09-10 16:52:33 +0300

    Made plugin buildable using `grails package-plugin`

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,22 @@
+grails.project.work.dir = 'target'
+grails.project.target.level = 1.6
+
+grails.project.dependency.resolution = {
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsHome()
+		grailsCentral()
+	}
+
+	dependencies {}
+
+	plugins {
+		build ':release:2.2.1',
+				':rest-client-builder:1.0.3', {
+			export = false
+		}
+	}
+}
+
