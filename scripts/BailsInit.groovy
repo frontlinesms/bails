9630241af86e34a1cbf128505883d830f7263f38
Alex Anderson <alex@frontlinesms.com>
2013-10-24 11:43:04 +0300

    Updated BailsInit to detect the plugin version automatically.

--- scripts/BailsInit.groovy
+++ scripts/BailsInit.groovy
@@ -1,5 +1,12 @@
 target(name:'bailsInit') {
-	def bailsVersion = '0.3.1'
+	def projectDir = new File(new File(getClass().protectionDomain.codeSource.location.path).parent).parent
+	def pluginDir
+	new File(projectDir, 'target/plugins').eachDirMatch(~/bails-.*/) {
+		pluginDir = it
+	}
+
+	def pluginDescriptorFile = new File(pluginDir, 'BailsGrailsPlugin.groovy')
+	def bailsVersion = new GroovyShell().parse("$pluginDescriptorFile.text; return new BailsGrailsPlugin().version").run()
 
 	symlink(link:"${basedir}/bails",
 			overwrite:true,
